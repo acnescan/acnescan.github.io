5a08868682b19853605bf4c76cb54f28722ce43f
gurk <gurkmandur@gmail.com>
2019-11-05 17:10:41 -0500

    display better results

--- backend.js
+++ backend.js
@@ -30,22 +30,39 @@ async function imagePOST(e) {
 function displayResults(result) {
 	// show results after 1 second
 	setTimeout(function () {
-		document.getElementById("loader").style.display = "none";
-
+		document.getElementById('loader').style.display = 'none';
 		const obj = JSON.parse(result);
-		const keys = Object.keys(obj);
-		const vals = Object.values(obj);
-		const max = Math.max.apply(null, vals);
-		const len = keys.length;
 
-		for (var i = 0; i < len; i++) {
-			if (vals[i] == max) {
-				document.getElementById(keys[i]).style.display = "block";
-			};
-			document.getElementById(keys[i] + "-score").innerHTML += vals[i];
-		};
+		// sort categories by highest value
+		let sortable = [];
+		for (let category in obj) {
+			// only add non negative categories
+			if (obj[category]) {
+   			sortable.push([category, obj[category]]);
+   		}
+		}
+		sortable.sort(function(a, b) {
+		    return b[1] - a[1];
+		});
 
-		document.getElementById("analysis").style.display = "block";
+		// display top 3 categories according to severity level
+		let displayCategories = Math.min(3, sortable.length);
+		console.log('displayCategories', displayCategories);
+
+		for (let i = 0; i < displayCategories; i++) {
+			const category = sortable[i][0];
+			const value = sortable[i][1];
+			let severity = document.getElementById(category).getElementsByClassName('severity')[0];
+			if (value < 0.4) {
+				severity.innerHTML = "We're not sure, but it looks like you have ";
+			} else if (value < 0.6) {
+				severity.innerHTML = "You might have ";
+			} else {
+				severity.innerHTML = "You have ";
+			}
+			document.getElementById(category).style.display = "block";
+		}
+
 	}, 1000)
 }
 
@@ -70,6 +87,15 @@ async function emailNotificationPOST(e) {
 		});
 		result = await response.json();
 		console.log(result);
+		
+		// display the success message 
+		if (response.ok) {
+			document.getElementById('email-submit-message').style.color = '#44c767';	// green
+			document.getElementById('email-submit-message').style.fontWeight = 'bold';
+			document.getElementById('email-submit-message').innerHTML = 'Added &#128516';
+		} else {
+			document.getElementById('email-submit-message').innerHTML = '';
+		}
 	} catch (error) {
 		console.log('error', error)
 	}
